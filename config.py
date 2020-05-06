42e7c268a5c09628a8fa826ff32de67c42146b10
Ryan Howard <ryan.howard@howqa.co.uk>
2020-05-06 14:07:27 +0100

    updated config.py with path to wkhtmltopdf

--- config.py
+++ config.py
@@ -5,3 +5,14 @@ class Config(object):
     SECRET_KEY = 'NOT_SO_SECRET'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    if 'DYNO' in os.environ:
+        print('loading wkhtmltopdf path on heroku')
+        WKHTMLTOPDF_CMD = subprocess.Popen(
+            ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')],
+            # Note we default to 'wkhtmltopdf' as the binary name
+            stdout=subprocess.PIPE).communicate()[0].strip()
+    else:
+        print('loading wkhtmltopdf path on localhost')
+        MYDIR = os.path.dirname(__file__)
+        WKHTMLTOPDF_CMD = os.path.join(MYDIR + "/static/executables/bin/", "wkhtmltopdf.exe")
